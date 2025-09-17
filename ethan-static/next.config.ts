86204168e69ecf78876b865862c747585d3b1ea4
Ethan <balik012@umn.edu>
2025-09-16 23:35:26 -0500

    galaxy based css

--- ethan-static/next.config.ts
+++ ethan-static/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src") // no need to directly reference the source folder
+        }
+    }
 };
 
 export default nextConfig;
